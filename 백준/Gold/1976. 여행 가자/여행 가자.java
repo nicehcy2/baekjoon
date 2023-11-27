fd2c91df6104b6776029584ef5bca8b316c3007b
Chan-Young Heo <105339362+nicehcy2@users.noreply.github.com>
2023-11-27 11:06:19 +0900

    [Gold IV] Title: 여행 가자, Time: 200 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int n, m;
+    public static int[] parent = new int[201];
+
+
+    public static void main(String[] args) throws Exception {
+        Input();
+        Solve();
+    }
+
+    static void Input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.valueOf(br.readLine());
+        m = Integer.valueOf(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 1; j <= n; j++) {
+                int edge = Integer.parseInt(st.nextToken());
+                if (edge == 1) {
+                    union(i, j);
+                }
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        int start = find(Integer.parseInt(st.nextToken()));
+        for (int i = 1; i < m; i++) {
+            int now = Integer.parseInt(st.nextToken());
+
+            if (start != find(now)) {
+                System.out.println("NO");
+                return;
+            }
+        }
+
+        System.out.println("YES");
+    }
+
+    static int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        } else {
+            return parent[x] = find(parent[x]);
+        }
+    }
+
+    static void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if (rootA != rootB) {
+            if (a < b) parent[rootB] = rootA;
+            else parent[rootA] = rootB;
+        }
+    }
+
+    static void Solve() throws Exception {
+    }
+}
