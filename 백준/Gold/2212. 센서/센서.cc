f0198d49ff6a43d937d58e220279fe66dfdc21bd
Chan-Young Heo <105339362+nicehcy2@users.noreply.github.com>
2024-01-29 18:38:17 +0900

    [Gold V] Title: 센서, Time: 4 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, K;
+int arr[100005];
+int diff[100004];
+
+void Solution() {
+    sort(arr, arr + N);
+
+    for (int i = 0; i < N - 1; i++) {
+        diff[i] = arr[i + 1] - arr[i];
+    }
+
+    sort(diff, diff + N - 1, greater<>());
+
+    int sum = 0;
+    for (int i = K - 1; i < N - 1; i++) {
+        sum += diff[i];
+    }
+
+    cout << sum << '\n';
+}
+
+void Input() {
+    cin >> N >> K;
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+}
+
+int main() {
+    Input();
+    Solution();
+
+    return 0;
+}
