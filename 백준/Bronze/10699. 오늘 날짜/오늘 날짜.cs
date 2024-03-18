44f8c0dbe804ab3ae6f9f46d1afb455b1331fdcd
Chan-Young Heo <105339362+nicehcy2@users.noreply.github.com>
2024-03-19 01:48:54 +0900

    [Bronze V] Title: 오늘 날짜, Time: 56 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2024-03-19");
+        }
+    }
+}
