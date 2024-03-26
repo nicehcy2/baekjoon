e86dfd197e88d725e34689b8a0f23dc989fe865c
Chan-Young Heo <105339362+nicehcy2@users.noreply.github.com>
2024-03-26 10:32:58 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,44 +1,44 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
 #include <cmath>
-#include <cstring>
-#include <stack>
-#include <string>
-#include <cstdlib>
-#include <sstream>
-#include <deque>
-#pragma warning(disable:4996)
+
 using namespace std;
 
-vector<int> dp;
+int N;
+int dp[1000010];
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+void Input() {
+	
+	cin >> N;
+}
 
-	int n;
-	cin >> n;
+void Solution() {
 
-	dp.resize(n + 1, 0);
+	dp[1] = 0, dp[2] = 1;
+	
+	for (int i = 2; i <= N; i++) {
 
-	dp[1] = 0;
-	dp[2] = 1;
-
-	for (int i = 2; i <= n; i++) {
 		dp[i] = dp[i - 1] + 1;
 
-		if (i % 2 == 0) {
-			dp[i] = min(dp[i], dp[i / 2] + 1);
-		}
-
 		if (i % 3 == 0) {
-			dp[i] = min(dp[i], dp[i / 3] + 1);
+			dp[i] = min(dp[i / 3] + 1, dp[i]);
+		}
+		
+		if (i % 2 == 0) {
+			dp[i] = min(dp[i / 2] + 1, dp[i]);
 		}
 	}
 
-	cout << dp[n];
+	cout << dp[N] << '\n';
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	Input();
+	Solution();
 
 	return 0;
 }
