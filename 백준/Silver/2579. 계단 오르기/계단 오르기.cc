26e685a13d4e30bd1105d2044ead2f0421c3b6c6
Chan-Young Heo <105339362+nicehcy2@users.noreply.github.com>
2024-03-27 10:01:29 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int dp[1010], arr[1010];
+
+void Input() {
+
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cin >> arr[i];
+	}
+}
+
+void Solution() {
+
+	dp[0] = 0, dp[1] = arr[1], dp[2] = max(arr[2], arr[1] + arr[2]);
+	dp[3] = max(arr[1] + arr[3], arr[2] + arr[3]);
+
+	for (int i = 4; i <= N; i++) {
+		dp[i] = max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i]);
+	}
+
+	cout << dp[N] << '\n';
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	Input();
+	Solution();
+
+	return 0;
+}
