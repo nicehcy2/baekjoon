c77ae4a29ed6b65bcf0ba9721509480b7acead05
Chan-Young Heo <105339362+nicehcy2@users.noreply.github.com>
2024-03-28 14:35:05 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -1,38 +1,40 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-int n;
-int arr[1001], dp[1001];
+int N;
+int dp[1002], arr[1001];
 
 void Input() {
-	cin >> n;
-	for (int i = 1; i <= n; i++) {
+	
+	cin >> N;
+	for (int i = 0; i < N; i++) {
 		cin >> arr[i];
 	}
 }
 
-void Solution() {
-	int max = -1;
 
-	for (int i = 1; i <= n; i++) {
+void Solution() {
+
+	for (int i = 0; i < N; i++) {
+		
 		dp[i] = arr[i];
-		for (int j = 1; j < i; j++) {
-			if (arr[j] < arr[i] && dp[i] < dp[j] + arr[i]) {
-				dp[i] = dp[j] + arr[i];
-			}
-		}
-		if (max < dp[i]) {
-			max = dp[i];
+		for (int j = 0; j < i; j++) {
+			
+			if (arr[i] > arr[j]) dp[i] = max(dp[j] + arr[i], dp[i]);
 		}
 	}
 
-	cout << max << '\n';
+	int ans = 0;
+	for (int i = 0; i < N; i++) {
+		ans = max(ans, dp[i]);
+	}
+
+	cout << ans << '\n';
 }
 
 int main() {
+
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
