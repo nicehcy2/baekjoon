7f6fb774c3b15ad92489b8b0c59e0ddafef60678
Chan-Young Heo <105339362+nicehcy2@users.noreply.github.com>
2024-03-29 13:01:56 +0900

    [Silver I] Title: 스티커, Time: 84 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -1,55 +1,56 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-int T, n;
-int dp[2][100001];
-int arr[2][100001];
-
-int mmax(int a, int b) {
-	return (a > b) ? a : b;
-}
-
-int Solution() {
-	dp[0][0] = 0;
-	dp[1][0] = 0;
-	dp[0][1] = arr[0][1];
-	dp[1][1] = arr[1][1];
-
-	for (int i = 2; i <= n; i++) {
-		//dp[0][i] = mmax(dp[1][i - 1] + arr[0][i], dp[0][i - 2] + arr[0][i]);
-		//dp[1][i] = mmax(dp[0][i - 1] + arr[1][i], dp[1][i - 2] + arr[1][i]);
-		dp[0][i] = mmax(dp[1][i - 1] + arr[0][i], mmax(dp[0][i - 2], dp[1][i - 2]) + arr[0][i]);
-		dp[1][i] = mmax(dp[0][i - 1] + arr[1][i], mmax(dp[0][i - 2], dp[1][i - 2]) + arr[1][i]);
-	}
-
-	return mmax(dp[0][n], dp[1][n]);
-}
+int T, N;
+int arr[2][100001], dp[2][100001];
 
 void Input() {
+}
+
+void Solution() {
+
 	cin >> T;
 
 	while (T--) {
-		cin >> n;
+
+		cin >> N;
+
+		memset(dp, 0, sizeof(dp));
+		
 		for (int i = 0; i < 2; i++) {
-			for (int j = 1; j <= n; j++) {
+			for (int j = 0; j < N; j++) {
 				cin >> arr[i][j];
 			}
 		}
 
-		int max = Solution();
-		cout << max << '\n';
+		// Soltuion
+		dp[0][0] = arr[0][0], dp[1][0] = arr[1][0];
+		dp[0][1] = dp[1][0] + arr[0][1], dp[1][1] = dp[0][0] + arr[1][1];
+
+		for (int i = 2; i < N; i++) {
+			dp[0][i] = max(dp[0][i - 2], dp[1][i - 2]) + arr[0][i];
+			dp[0][i] = max(dp[0][i], dp[1][i - 1] + arr[0][i]);
+
+			dp[1][i] = max(dp[0][i - 2], dp[1][i - 2]) + arr[1][i];
+			dp[1][i] = max(dp[1][i], dp[0][i - 1] + arr[1][i]);
+		}
+
+		int ans = max(dp[0][N - 1], dp[1][N - 1]);
+
+		cout << ans << '\n';
 	}
 }
 
-
-
 int main() {
+
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
 	Input();
+	Solution();
 
 	return 0;
 }
