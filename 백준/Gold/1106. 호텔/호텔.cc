90ae8b01c0cb35109ea89eb5ec1ebd3d16a82527
Chan-Young Heo <105339362+nicehcy2@users.noreply.github.com>
2024-04-02 10:05:09 +0900

    [Gold V] Title: 호텔, Time: 4 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.cc
+++ 백준/Gold/1106. 호텔/호텔.cc
@@ -1,44 +1,52 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int C, N;
-vector<pair<int, int>> v;
+pair<int, int> arr[21];
 int dp[100001];
 
+void Input() {
+
+	cin >> C >> N;
+
+	for (int i = 1; i <= N; i++) {
+
+		cin >> arr[i].first >> arr[i].second;
+	}
+}
+
 void Solution() {
-	for (int i = 0; i < v.size(); i++) {
-		for (int j = 1; j < 100001; j++) {
-			if (j - v[i].first >= 0) {
-				dp[j] = max(dp[j], dp[j - v[i].first] + v[i].second);
+
+	// dp[i] : i번째 비용으로 얻을 수 있는 고객의 최대값
+	for (int i = 1; i <= 20; i++) {
+
+		dp[arr[i].first] = arr[i].second;
+
+		for (int j = 1; j <= 100001; j++) {
+			
+			if (j - arr[i].first >= 0) {
+				dp[j] = max(dp[j], dp[j - arr[i].first] + arr[i].second);
 			}
 		}
 	}
 
-	for (int i = 1; i <= 100001; i++)
-	{
-		if (dp[i] >= C)
-		{
+	for (int i = 1; i <= 100001; i++) {
+
+		// cout << "dp[" << i << "] = " << dp[i] << '\n';
+		if (dp[i] >= C) {
 			cout << i << '\n';
 			break;
 		}
 	}
 }
 
-void Input() {
-	int m, h;
-	cin >> C >> N;
-	
-	for (int i = 0; i < N; i++) {
-		cin >> m >> h;
-
-		v.push_back({ m, h });
-	}
-}
-
 int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	Input();
 	Solution();
 
