fc7b8b05b99d4d91586e112b59ed65ee7b29feb4
Chan-Young Heo <105339362+nicehcy2@users.noreply.github.com>
2024-05-14 10:39:52 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 128 ms, Memory: 10228 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -4,47 +4,42 @@ using namespace std;
 
 int N, M;
 int arr[1025][1025];
-
-void Input() {
-
-	cin >> N >> M;
-
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-
-			cin >> arr[i][j];
-
-			arr[i][j] += arr[i - 1][j];
-		}
-	}
-}
+int prefixSum[1025][1025];
 
 void Solution() {
 
-	int x1, y1, x2, y2, sum = 0;
+    while (M--) {
 
-	for (int i = 0; i < M; i++) {
-		
-		cin >> y1 >> x1 >> y2 >> x2;
+        int x1, y1, x2, y2;
+        cin >> y1 >> x1 >> y2 >> x2;
 
-		for (int x = x1; x <= x2; x++) {
+        cout << prefixSum[y2][x2] - prefixSum[y2][x1 - 1] - prefixSum[y1 - 1][x2] + prefixSum[y1 - 1][x1 - 1] << '\n';
+    }
+}
 
-			sum += (arr[y2][x] - arr[y1 - 1][x]);
-		}
+void Input() {
 
-		cout << sum << '\n';
-		sum = 0;
-	}
+    cin >> N >> M;
+
+    for (int i = 1; i <= N; i++) {
+
+        for (int j = 1; j <= N; j++) {
+
+            cin >> arr[i][j];
+
+            prefixSum[i][j] = arr[i][j] + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1];
+        }
+    }
 }
 
 int main() {
 
-	ios::sync_with_stdio(false);	
-	cin.tie(NULL);
-	cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	Input();
-	Solution();
+    Input();
+    Solution();
 
-	return 0;
+    return 0;
 }
