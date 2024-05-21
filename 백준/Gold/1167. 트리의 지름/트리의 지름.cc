1303d4466108c49966b935f161d2260113676d04
Chan-Young Heo <105339362+nicehcy2@users.noreply.github.com>
2024-05-21 11:53:30 +0900

    [Gold II] Title: 트리의 지름, Time: 76 ms, Memory: 8644 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.cc
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -1,67 +1,71 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
-#include <queue>
-#include <map>
 
 using namespace std;
 
-int n;
-vector<pair<int, int>> v[1000002];
-bool visited[100002];
-int maxDistance = 0;
-int maxNode = 0;
+int V;
+vector<pair<int, int>> g[100001];
+bool visited[100001];
 
-void InitAndInput() {
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		int start;
-		cin >> start;
+int diameter, maxDist = 0;
 
-		while (true) {
-			int end, distance;
-			cin >> end;
-			if (end == -1) break;
-			cin >> distance;
+void dfs(int n, int dist) {
 
-			v[start].push_back({ distance, end });
-		}
-	}
+    if (maxDist < dist) {
+        maxDist = dist;
+        diameter = n;
+    }
+    
+    visited[n] = true;
+
+    for (int i = 0; i < g[n].size(); i++) {
+
+        if (!visited[g[n][i].first]) {
+            dfs(g[n][i].first, dist + g[n][i].second);
+        }
+    }
 }
-
-void dfs(int start, int distance) {
-	if (distance > maxDistance) {
-		maxDistance = distance;
-		maxNode = start;
-	}
-
-	for (int i = 0; i < v[start].size(); i++) {
-		if (!visited[v[start][i].second]) {
-			visited[v[start][i].second] = true;
-			dfs(v[start][i].second, distance + v[start][i].first);
-		}
-	}
-}
-
-
 
 void Solution() {
-	visited[1] = true;
-	dfs(1, 0);
-	memset(visited, 0, sizeof(visited));
-	visited[maxNode] = true;
-	dfs(maxNode, 0);
 
-	cout << maxDistance << '\n';
+    dfs(1, 0);
+    memset(visited, 0, sizeof(visited));
+    dfs(diameter, 0);
+
+    cout << maxDist << '\n';
+}
+
+void Input() {
+
+    cin >> V;
+
+    for (int i = 1; i <= V; i++) {
+        
+        int start; cin >> start;
+
+        while (true) {
+            int edge, cost;
+
+            cin >> edge;
+            if (edge == -1) {
+                break;
+            }
+            cin >> cost;
+
+            g[start].push_back({ edge, cost });
+        }
+    }
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
 
-	InitAndInput();
-	Solution();
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    Input();
+    Solution();
 
-	return 0;
+    return 0;
 }
