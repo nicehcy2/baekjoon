85cd471253cfe8fb0d45f68626f7a8c9679c5e6a
Chan-Young Heo <105339362+nicehcy2@users.noreply.github.com>
2024-05-24 14:24:27 +0900

    [Silver I] Title: 기타 레슨, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int arr[100001];
+
+void Solution() {
+
+    int maxVl = 0, sum = 0, start = 0, end = 0, mid = 0;
+
+    for (int i = 0; i < N; i++) {
+        maxVl = max(arr[i], maxVl);
+        sum += arr[i];
+    }
+
+    start = maxVl, end = sum;
+
+    while (end >= start) {
+
+        mid = (start + end) / 2;
+
+        int cnt = 0, pSum = 0;
+        for (int i = 0; i < N; i++) {
+
+            if (pSum + arr[i] > mid) {
+                pSum = 0;
+                cnt++;
+            }
+            pSum += arr[i];
+        }
+        if (pSum != 0) cnt++;
+
+        if (cnt <= M) {
+            end = mid - 1;
+        }
+        else {
+            start = mid + 1;
+        }
+    }
+
+    cout << start << '\n';
+}
+
+void Input() {
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+}
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    Input();
+    Solution();
+
+    return 0;
+}
