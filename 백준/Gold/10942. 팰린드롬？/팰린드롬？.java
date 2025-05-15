1e4009e359696cfab06f03bdc05a3c7b1c6713f2
Chan-Young Heo <105339362+nicehcy2@users.noreply.github.com>
2025-05-16 00:20:17 +0900

    [Gold IV] Title: 팰린드롬?, Time: 792 ms, Memory: 230560 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int[] num;
+	static int[][] dp;
+    
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		num = new int[n+1];
+		dp = new int[n+1][n+1];
+		for(int i=0; i< n+1; i++) {
+			Arrays.fill(dp[i], -1);
+		}
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=1; i<n+1; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int t = Integer.parseInt(br.readLine());
+		for(int i=0; i<t; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+            
+			boolean flag = checkPalin(start, end) == 1? true : false;
+			if(flag) {
+				sb.append("1\n");
+			}else {
+				sb.append("0\n");
+			}
+		}
+		
+		System.out.println(sb.toString());
+	}
+
+	static int checkPalin(int s, int e) {
+		if( s >= e) return 1;
+		
+		if(dp[s][e] != -1) return dp[s][e];
+		
+		if(num[s]==num[e]) {
+			return dp[s][e] = checkPalin(s+1, e-1);
+		}
+		return 0;
+	
+	}
+}
