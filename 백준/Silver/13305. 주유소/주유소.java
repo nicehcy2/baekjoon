feb9ba690fe8416638e3856cf395564befeadfca
Chan-Young Heo <105339362+nicehcy2@users.noreply.github.com>
2025-07-08 16:33:07 +0900

    [Silver III] Title: 주유소, Time: 280 ms, Memory: 30468 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N;
+	static long result = 0;
+	static int[] fuels, roads;
+
+	public static void main(String args[]) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+		roads = new int[N - 1];
+		fuels = new int[N - 1];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N - 1; i++) {
+			roads[i] = Integer.parseInt(st.nextToken());
+		}
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N - 1; i++) {
+			fuels[i] = Integer.parseInt(st.nextToken());
+		}
+
+		result += fuels[0] * roads[0];
+		int cur = fuels[0];
+		int count = 1;
+		while (count < N - 1) {
+
+			if (cur > fuels[count]) {
+				cur = fuels[count];
+			}
+			result += cur * roads[count++];
+		}
+
+		System.out.println(result);
+	}
+}
