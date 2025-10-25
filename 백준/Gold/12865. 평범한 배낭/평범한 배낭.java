0393a6e0ac0dc9a0af20404a3859f36a9a601233
Chan-Young Heo <105339362+nicehcy2@users.noreply.github.com>
2025-10-26 00:00:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 160 ms, Memory: 53768 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -4,45 +4,41 @@ import java.io.*;
 public class Main {
 
     static int N, K;
-    static List<int[]> list;
-    static int[][] dp;
+    static int[][] arr;
+    static int[][] P;
 
-    static void solve() throws Exception {
+    static void solve() {
+
+        for (int i=1; i<=N; i++) {
+            for (int j=1; j<=K; j++) {
+                int W = arr[i][0];
+                int V = arr[i][1];
+
+                if (j < W) P[i][j] = P[i - 1][j];
+                else P[i][j] = Math.max(P[i - 1][j], V + P[i - 1][j - W]);
+            }
+        }
+
+        System.out.println(P[N][K]);
+    }
+
+    public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-
-        list = new ArrayList<>();
-        dp = new int[N + 1][K + 1];
+        arr = new int[N + 1][2];
+        P = new int[N + 1][K + 1];
 
         for (int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
             int W = Integer.parseInt(st.nextToken());
             int V = Integer.parseInt(st.nextToken());
-            list.add(new int[]{W, V});
-
-            for (int j=W; j<=K; j++) {
-                dp[i][j] = V;
-            }
+            arr[i + 1][0] = W;
+            arr[i + 1][1] = V;
         }
-
-        for (int i=1; i<N; i++) {
-            for (int j = 1; j <= K; j++) {
-                if (j - list.get(i)[0] >= 0) {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j] + dp[i - 1][j - list.get(i)[0]]);
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
-
-        System.out.println(dp[N - 1][K]);
-    }
-
-    public static void main(String[] args) throws Exception {
 
         solve();
     }
