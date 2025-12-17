5ca763eed5abbe71ed4035bf946e8fcf8a62b1d7
Chan-Young Heo <105339362+nicehcy2@users.noreply.github.com>
2025-12-17 18:45:47 +0900

    [Gold IV] Title: 점수따먹기, Time: 312 ms, Memory: 36516 KB -BaekjoonHub

--- 백준/Gold/1749. 점수따먹기/점수따먹기.java
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.java
@@ -5,22 +5,37 @@ public class Main {
 
     static int N, M;
     static int[][] prefixSum;
+    static int[] rowSum;
 
     static void solve() {
 
         int max = Integer.MIN_VALUE;
-        for (int i=1; i<=N; i++) {
-            for (int j=1; j<=i; j++) {
-                for (int k=1; k<=M; k++) {
-                    for (int l=1; l<=k; l++) {
 
-                        max = Integer.max(max, prefixSum[i][k] + prefixSum[j - 1][l - 1] - prefixSum[i][l - 1] - prefixSum[j - 1][k]);
-                    }
+        for (int i=1; i<=M; i++) {
+            for (int j=i; j<=M; j++) {
+                for (int row = 1; row<=N; row++) {
+
+                    rowSum[row] = prefixSum[row][j] - prefixSum[row][i - 1];
                 }
+
+                int cur = kadane();
+                max = Math.max(max, cur);
             }
         }
 
         System.out.println(max);
+    }
+
+    static int kadane() {
+
+        int[] dp = new int[N + 1];
+        dp[0] = 0;
+        int max = rowSum[1];
+        for (int i=1; i<=N; i++) {
+            dp[i] = Math.max(rowSum[i], dp[i - 1] + rowSum[i]);
+            max = Math.max(dp[i], max);
+        }
+        return max;
     }
 
     public static void main(String[] args) throws Exception {
@@ -29,14 +44,14 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         prefixSum = new int[N + 1][M + 1];
+        rowSum = new int[N + 1];
 
         for (int i=0; i<N; i++) {
 
             st = new StringTokenizer(br.readLine());
             for (int j=0; j<M; j++) {
-                prefixSum[i + 1][j + 1] = prefixSum[i][j + 1] + prefixSum[i + 1][j] - prefixSum[i][j] + Integer.parseInt(st.nextToken());
+                prefixSum[i + 1][j + 1] = prefixSum[i + 1][j] + Integer.parseInt(st.nextToken());
             }
         }
 
