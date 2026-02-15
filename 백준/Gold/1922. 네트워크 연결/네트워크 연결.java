ee9c606ddb47c76217c96cdcea506d3f3517a9c7
Chan-Young Heo <105339362+nicehcy2@users.noreply.github.com>
2026-02-15 18:27:31 +0900

    [Gold IV] Title: 네트워크 연결, Time: 504 ms, Memory: 50084 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N, M, result = 0;
+    static int[][] graph;
+    static int[] parent;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        graph = new int[M][3];
+        parent = new int[N + 1];
+
+        for (int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            graph[i][0] = Integer.parseInt(st.nextToken());
+            graph[i][1] = Integer.parseInt(st.nextToken());
+            graph[i][2] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(graph, (o1, o2) -> Integer.compare(o1[2], o2[2]));
+        kruskal();
+
+        System.out.println(result);
+        br.close();
+    }
+
+    static int find(int x) {
+
+        if (parent[x] == x) return x;
+        else return parent[x] = find(parent[x]);
+    }
+
+    static void union(int x, int y) {
+
+        x = find(x);
+        y = find(y);
+
+        if (x > y) parent[x] = y;
+        else parent[y] = x;
+    }
+
+    static void kruskal() {
+
+        for (int i=1; i<=N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i=0; i<M; i++) {
+
+            if (find(graph[i][0]) != find(graph[i][1])) {
+                result += graph[i][2];
+                union(graph[i][0], graph[i][1]);
+            }
+        }
+    }
+}
