70aded8d2b2b0cf5a9c1f05517c35ef10e3cc1a1
Chan-Young Heo <105339362+nicehcy2@users.noreply.github.com>
2026-03-04 15:09:40 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 580 ms, Memory: 68848 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,12 +1,10 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int N, E, V1, V2;
+    static int N, E;
     static List<int[]>[] graph;
-    static boolean[] visited;
-    static int[] dist;
 
     public static void main(String[] args) throws Exception {
 
@@ -15,28 +13,21 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
-        graph = new ArrayList[N + 1];
-        visited = new boolean[N + 1];
-        dist = new int[N + 1];
-
-        for (int i=1; i<=N; i++) graph[i] = new ArrayList<>();
+        graph = new List[N + 1];
+        for (int i=1; i<= N; i++) graph[i] = new ArrayList<>();
 
         for (int i=0; i<E; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-
-            graph[s].add(new int[]{e, cost});
-            graph[e].add(new int[]{s, cost});
+            int c = Integer.parseInt(st.nextToken());
+            graph[s].add(new int[]{e, c});
+            graph[e].add(new int[]{s, c});
         }
         st = new StringTokenizer(br.readLine());
-        V1 = Integer.parseInt(st.nextToken());
-        V2 = Integer.parseInt(st.nextToken());
+        int V1 = Integer.parseInt(st.nextToken());
+        int V2 = Integer.parseInt(st.nextToken());
 
-        // 주의사항: V1과 V2의 순서는 상관없다. 더 적합한걸 방문해야됨.
-        // 1 -> V1 -> V2 -> N
-        // 1 -> V2 -> V1 -> N
         int result1 = 0, result2 = 0, result;
 
         result1 += dijkstra(1, V1);
@@ -56,10 +47,11 @@ public class Main {
 
     static int dijkstra(int start, int target) {
 
-        Arrays.fill(visited, false);
+        boolean[] visited = new boolean[N + 1];
+        int[] dist = new int[N + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
         pq.add(new int[]{start, 0});
         dist[start] = 0;
 
@@ -67,11 +59,11 @@ public class Main {
             int[] cur = pq.poll();
             int curVertex = cur[0];
 
-            if (visited[curVertex]) continue;;
-
+            if (visited[curVertex]) continue;
             visited[curVertex] = true;
 
             for (int i=0; i<graph[curVertex].size(); i++) {
+
                 int nextVertex = graph[curVertex].get(i)[0];
                 int nextCost = graph[curVertex].get(i)[1];
 
